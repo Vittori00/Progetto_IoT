d9b3e422173edf1b1ad34ee034ed9cd1ad06cd9c
Vittori00 <45265824+Vittori00@users.noreply.github.com>
2024-05-24 11:09:43 +0200

    Update res_co2.c

--- illumation/resources/res_co2.c
+++ illumation/resources/res_co2.c
@@ -5,24 +5,54 @@
 #include <string.h>
 #include "coap-engine.h"
 
-
+// Function declarations
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_event_handler(void);
+void initialize_co2_level(void);
 
-RESOURCE(res_co2,
-                  "title=\"CO2\";rt=\"\";obs",
-                  res_get_handler,
-                  NULL,
-                  NULL,
-                  NULL);
+// Global variables
+int co2_level = 1200;
+int too_high = 1500;
+int too_low = 400;
+bool switched = 0;
+int counter = 0;
 
-static void
-res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
-{
-    int co2_level = (float)(rand() % 1800 + 200);   
+// Event resource definition
+EVENT_RESOURCE(res_co2,
+               "title=\"CO2\";rt=\"\";obs",
+               res_get_handler,
+               NULL,
+               NULL,
+               NULL,
+               res_event_handler);
 
+// Initialize the CO2 level
+void initialize_co2_level(void) {
+    co2_level = (rand() % 1800) + 200;
+}
+
+// Event handler function
+static void res_event_handler(void) {
+    if (co2_level == 0 || counter == 2 || switched) {
+        initialize_co2_level();
+        counter = 0;
+        switched = false;
+    }
+
+    // Check if CO2 level is out of range
+    if (co2_level < too_low || co2_level > too_high) {
+        switched = true;
+        // Notify all the observers
+        coap_notify_observers(&res_co2);
+    }
+
+    counter++;
+}
+
+// GET handler function
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
     coap_set_header_content_format(response, APPLICATION_JSON);
     snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"co2_level\": %d}", co2_level);
     coap_set_payload(response, buffer, strlen((char *)buffer));
-
 }
 
