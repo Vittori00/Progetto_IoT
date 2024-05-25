34f74f2d5a67e4017336435b98aa85d7150eb24e
christianp12 <krys.petruz@yahoo.it>
2024-05-25 22:27:20 +0200

    res_luce update (res_post_handler)

--- illumination/resources/res_luce.c
+++ illumination/resources/res_luce.c
@@ -5,17 +5,25 @@
 #include <string.h>
 #include "coap-engine.h"
 
+// Function declarations
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_post_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void res_event_handler(void);
+
+// Global variables
 int light = 0;
+
+// Event resource definition
 EVENT_RESOURCE(res_luce,
                "title=\"Light\";rt=\"\";obs",
                res_get_handler,
-               NULL,
+               res_post_handler,
                NULL,
                NULL,
                res_event_handler);
 
+
+// Event handler function
 static void
 res_event_handler(void)
 {
@@ -29,10 +37,28 @@ res_event_handler(void)
     light = new_light;
 }
 
+
+// GET handler function
 static void
 res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
+    coap_set_header_content_format(response, APPLICATION_JSON);
+    snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"light\": %d}", light);
+    coap_set_payload(response, buffer, strlen((char *)buffer));
+}
 
+
+// POST handler function
+static void
+res_post_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+    // Alternate the light value
+    light = (light == 0) ? 1 : 0;
+
+    // Notify all the observers about the light change
+    coap_notify_observers(&res_luce);
+
+    // Respond with the updated light
     coap_set_header_content_format(response, APPLICATION_JSON);
     snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"light\": %d}", light);
     coap_set_payload(response, buffer, strlen((char *)buffer));
