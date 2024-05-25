df5548a9b2255bc5edc226b4698db0dc944467de
Vittori00 <45265824+Vittori00@users.noreply.github.com>
2024-05-25 11:23:44 +0200

    Update res_co2.c

--- illumation/resources/res_co2.c
+++ illumation/resources/res_co2.c
@@ -11,7 +11,7 @@ static void res_event_handler(void);
 void initialize_co2_level(void);
 
 // Global variables
-int co2_level = 1200;
+int co2_level = 0;
 int too_high = 1500;
 int too_low = 400;
 bool switched = 0;
@@ -27,32 +27,34 @@ EVENT_RESOURCE(res_co2,
                res_event_handler);
 
 // Initialize the CO2 level
-void initialize_co2_level(void) {
+void initialize_co2_level(void)
+{
     co2_level = (rand() % 1800) + 200;
 }
 
 // Event handler function
-static void res_event_handler(void) {
-    if (co2_level == 0 || counter == 2 || switched) {
+static void res_event_handler(void)
+{
+    if (co2_level == 0 || counter == 2 || switched)
+    {
         initialize_co2_level();
         counter = 0;
         switched = false;
+        coap_notify_observers(&res_co2);
     }
-
-    // Check if CO2 level is out of range
-    if (co2_level < too_low || co2_level > too_high) {
+    else if (co2_level < too_low || co2_level > too_high) // Check if CO2 level is out of range
+    {
         switched = true;
         // Notify all the observers
         coap_notify_observers(&res_co2);
     }
-
     counter++;
 }
 
 // GET handler function
-static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
     coap_set_header_content_format(response, APPLICATION_JSON);
     snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"co2_level\": %d}", co2_level);
     coap_set_payload(response, buffer, strlen((char *)buffer));
 }
-
