cb0b6dfb0f874981398bb8a43e7aa27082bd847a
christianp12 <krys.petruz@yahoo.it>
2024-05-25 22:20:20 +0200

    res_phase update (res_post_handler)

--- illumination/resources/res_phase.c
+++ illumination/resources/res_phase.c
@@ -5,17 +5,25 @@
 #include <string.h>
 #include "coap-engine.h"
 
+// Function declarations
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_post_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void res_event_handler(void);
+
+// Global variables
 int phase = 0;
+
+// Event resource definition
 EVENT_RESOURCE(res_phase,
                "title=\"Phase\";rt=\"\";obs",
                res_get_handler,
-               NULL,
+               res_post_handler,
                NULL,
                NULL,
                res_event_handler);
 
+
+// Event handler function
 static void
 res_event_handler(void)
 {
@@ -29,10 +37,30 @@ res_event_handler(void)
     }
     phase = new_phase;
 }
+
+
+// GET handler function
 static void
 res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
 
+    coap_set_header_content_format(response, APPLICATION_JSON);
+    snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"phase\": %d}", phase);
+    coap_set_payload(response, buffer, strlen((char *)buffer));
+}
+
+
+// POST handler function
+static void
+res_post_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+    // Alternate the phase value
+    phase = (phase == 0) ? 1 : 0;
+
+    // Notify all the observers about the phase change
+    coap_notify_observers(&res_phase);
+
+    // Respond with the updated phase
     coap_set_header_content_format(response, APPLICATION_JSON);
     snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"phase\": %d}", phase);
     coap_set_payload(response, buffer, strlen((char *)buffer));
