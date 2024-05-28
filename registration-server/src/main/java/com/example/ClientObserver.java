e538fab48c670080fb9e283d04f0d04d0afd3949
Christian Petruzzella <krys.petruz@yahoo.it>
2024-05-28 23:09:27 +0200

    CoapObserver for Illumination Sensor

--- registration-server/src/main/java/com/example/ClientObserver.java
+++ registration-server/src/main/java/com/example/ClientObserver.java
@@ -1,37 +1,64 @@
 package com.example;
 
 import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapHandler;
 import org.eclipse.californium.core.CoapObserveRelation;
 import org.eclipse.californium.core.CoapResponse;
-import org.eclipse.californium.core.CoapHandler;
+import org.json.JSONObject;
 
+import com.example.Resources.IlluminationResource;
 public class ClientObserver {
+	private CoapClient client;
+	private DBManager dbManager = new DBManager("jdbc:mysql://localhost:3306/measures", "admin", "admin");
+	private IlluminationResource resource;
 
-	public static void main(String[] args) {
+	public ClientObserver(IlluminationResource resource) {
+		client = new CoapClient("coap://[" + resource.getSensorAddress() + "]/" + resource.getResourceName());
+		this.resource = resource;
+	}
 
-		CoapClient client = new CoapClient("coap://localhost/observe");
-
-		CoapResponse response = client.get();
-
-		System.out.print(response.getResponseText());
-
-		CoapObserveRelation relation = client.observe(
-			new CoapHandler() {
-				@Override
-				public void onLoad(CoapResponse response) {
-					String content = response.getResponseText();
-					System.out.println(content);
+	public void startObserving(){
+		CoapObserveRelation relation = client.observe(new CoapHandler() {
+			public void onLoad(CoapResponse response) {
+				JSONObject json = new JSONObject(response.getResponseText());
+				if(json.has("CO2")){
+					String resouceName = "CO2";
+					int CO2 = json.getInt("CO2");
+					int timestamp = json.getInt("timestamp");
+					dbManager.insert("Illumination Sensor", resource.getSensorAddress(), resouceName, CO2, timestamp);
+					System.out.println("New CO2 of " + CO2 + "ppm registered at " + timestamp);
 				}
-
-				@Override
-				public void onError() {
-					System.err.println("-Failed--------");
+				else if(json.has("Light")){
+					String resouceName = "Light";
+					int Light = json.getInt("Light");
+					int timestamp = json.getInt("timestamp");
+					dbManager.insert("Illumination Sensor", resource.getSensorAddress(), resouceName, Light, timestamp);
+					if (Light == 1) {
+						System.out.println("New Light phase registered at " + timestamp);
+					} else {
+						System.out.println("New Dark phase registered at " + timestamp);
+					}
 				}
-			});
-		try {
-			Thread.sleep(6 * 1000);
-		} catch (InterruptedException e) {
-		}
-		relation.proactiveCancel(); // to cancel observing
+				else if(json.has("Phase")){
+					String resouceName = "Phase";
+					int Phase = json.getInt("Phase");
+					int timestamp = json.getInt("timestamp");
+					dbManager.insert("Illumination Sensor", resource.getSensorAddress(), resouceName, Phase, timestamp);
+					if (Phase == 0) {
+						System.out.println("New Farm Phase 0 registered at " + timestamp);
+					} else {
+						System.out.println("New Farm Phase 1 registered at " + timestamp);
+					}
+				}
+				else {
+					System.out.println("Error: wrong message format from Illumination sensor ");
+				}
+			}
+
+			public void onError() {
+				System.out.println("Nothing to observe");
+			}
+		});
+		relation.proactiveCancel();
 	}
 }
---
+++ registration-server/src/main/java/com/example/DBManager.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import java.sql.*;
+
+public class DBManager {
+    private String DB_URL;
+    private String user;
+    private String password;
+    private PreparedStatement pstInsert;
+
+    public DBManager(String URL, String user, String password){
+        this.DB_URL = URL;
+        this.user = user;
+        this.password = password;
+
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, this.user, this.password);
+            pstInsert = conn.prepareStatement(
+                "INSERT INTO measures (sensIP, temperature, timestamp) " +
+                    "VALUES (?, ?, ?);"
+            );
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void insert(String sensorName, String sensorAddress, String resourceName, int resourceValue, int timestamp){
+        try{
+            pstInsert.setString(2, sensorName);
+            pstInsert.setString(3, sensorAddress);
+            pstInsert.setString(4, resourceName);
+            pstInsert.setInt(5, resourceValue);
+            pstInsert.setInt(6, timestamp);
+            pstInsert.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
