a8bf6b3977aa53892b93dceb17e38eb0d61c7dd4
Vittori00 <lorenzo.vittori24@gmail.com>
2024-05-29 23:34:23 +0200

    ssto sync

--- illumination/resources/res_light.c
+++ illumination/resources/res_light.c
@@ -10,7 +10,6 @@ static void res_get_handler(coap_message_t *request, coap_message_t *response, u
 static void res_post_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void res_event_handler(void);
 
-// Global variables
 int light = 0;
 
 // Event resource definition
