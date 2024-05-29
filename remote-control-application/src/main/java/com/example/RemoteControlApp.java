8e5871de97c910b3dae11699dd63dd55be2d3cd9
Christian Petruzzella <krys.petruz@yahoo.it>
2024-05-29 23:13:43 +0200

    RemoteControlApp Update

--- remote-control-application/src/main/java/com/example/RemoteControlApp.java
+++ remote-control-application/src/main/java/com/example/RemoteControlApp.java
@@ -4,9 +4,17 @@ import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
 import org.json.JSONObject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class RemoteControlApp {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/CottonNet";
+    private static final String DB_USER = "admin";
+    private static final String DB_PASSWORD = "admin";
 
     private static final String COAP_ACTUATORS_URI = "coap://localhost/actuators";
     private static final String COAP_SENSORS_URI = "coap://localhost/sensors";
@@ -18,16 +26,13 @@ public class RemoteControlApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        CoapClient sensors = new CoapClient(COAP_SENSORS_URI);
         CoapClient actuators = new CoapClient(COAP_ACTUATORS_URI);
+        CoapClient sensors = new CoapClient(COAP_SENSORS_URI);
 
         while (true) {
             System.out.println("Remote Control Application");
-            System.out.println("1. Turn on a device");
-            System.out.println("2. Turn off a device");
-            System.out.println("3. Show status of sensors");
-            System.out.println("4. Show status of actuators");
-            System.out.println("5. Set new sample timing");
+            System.out.println("1. Show active devices");
+            System.out.println("2. Set new sample timing");
             System.out.print("\nScegli un'opzione: ");
 
             int choice = scanner.nextInt();
@@ -35,35 +40,10 @@ public class RemoteControlApp {
 
             switch (choice) {
                 case 1:
-                    // Turn on device
-                    System.out.print("Insert type of sensor to turn on (sensor/actuator): ");
-                    String type = scanner.nextLine();
-                    System.out.print("Insert name of sensor to turn on: ");
-                    String name = scanner.nextLine();
-                    if (type.equals("sensor")) {
-                        System.out.print("Insert sampling time: ");
-                        int sampling = scanner.nextInt();
-                        scanner.nextLine();
-                        turnOnDevice(sensors, name, sampling);
-                    } else if (type.equals("actuator")) {
-                        turnOnDevice(actuators, name, 0);
-                    }
+                    // Show active devices
+                    getActiveDevices();
                     break;
                 case 2:
-                    // Turn off a device
-                    System.out.print("Insert sensor to turn off: ");
-                    String sensorNameOff = scanner.nextLine();
-                    //turnOffDevice(sensors, sensorNameOff);
-                    break;
-                case 3:
-                    // Show status of sensors
-                    getStatus(sensors);
-                    break;
-                case 4:
-                    // Show status of actuators
-                    getStatus(actuators);
-                    break;
-                case 5:
                     // Choose resource
                     System.out.println("Choose a resource:");
                     System.out.println("1. Illumination");
@@ -93,45 +73,6 @@ public class RemoteControlApp {
         }
     }
 
-    private static void turnOnDevice(CoapClient client, String name, int sampling) {
-        JSONObject json = new JSONObject();
-
-        json.put("name", name);
-        if (sampling == 0) {
-            json.put("type", "actuator");
-        } else {
-            json.put("type", "sensor");
-            json.put("sampling", sampling);
-        }
-
-        CoapResponse response = client.post(json.toString(), 0);
-        if (response != null) {
-            System.out.println("Response: " + response.getResponseText());
-        } else {
-            System.out.println("No response from server.");
-        }
-    }
-
-
-    private static void getStatus(CoapClient client) {
-        CoapResponse response = client.get();
-        if (client.getURI().equals(COAP_SENSORS_URI)) {
-            if (response != null) {
-                System.out.println("Sensors overview" + response.getResponseText());
-            } else {
-                System.out.println("No response from server.");
-            }            
-        } else if (client.getURI().equals(COAP_ACTUATORS_URI)) {
-            if (response != null) {
-                System.out.println("Actuators overview: " + response.getResponseText());
-            } else {
-                System.out.println("No response from server.");
-            }
-        } else {
-            System.out.println("No response from server.");
-        }
-    }
-
     private static void setIlluminationSampling(int illuminationSampling) {
 
         CoapClient client = new CoapClient(ILLUMINATION_RESOURCE_URI);
@@ -144,11 +85,43 @@ public class RemoteControlApp {
         } else {
             System.out.println("No response from server.");
         }
-
     }
 
     private static void setSprinklerSampling(int sprinklerSampling) {
         CoapClient client = new CoapClient(SPRINKLER_RESOURCE_URI);
+        JSONObject json = new JSONObject();
+        json.put("sampling", sprinklerSampling);
 
+        CoapResponse response = client.post(json.toString(), 0);
+        if (response != null) {
+            System.out.println("Response: " + response.getResponseText());
+        } else {
+            System.out.println("No response from server.");
+        }
+    }
+
+
+
+    private static void getActiveDevices(){
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM devices");
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String address = resultSet.getString("address");
+                String type = resultSet.getString("type");
+                int sampling = resultSet.getInt("sampling");
+
+                System.out.println("name: " + name + ", address: " + address + ", type: " + type + ", sampling: " + sampling);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
