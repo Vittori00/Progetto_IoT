c9aca1ca8d9ef29ac8034e9e4b707b7537c9a191
Vittori00 <lorenzo.vittori24@gmail.com>
2024-06-02 18:45:25 +0200

    debug

---
+++ CoAP network/lighting controller/build/cooja/obj/.deps/cJSON.c.d
@@ -0,0 +1,4 @@
+build/cooja/obj/cJSON.o: ../cJSON/cJSON.c \
+ ../../../../os/net/app-layer/coap/module-macros.h ../cJSON/cJSON.h
+../../../../os/net/app-layer/coap/module-macros.h:
+../cJSON/cJSON.h:
---
+++ CoAP network/soil sensor/build/cooja/obj/.deps/cJSON_Utils.c.d
@@ -0,0 +1,6 @@
+build/cooja/obj/cJSON_Utils.o: ../cJSON/cJSON_Utils.c \
+ ../../../../os/net/app-layer/coap/module-macros.h ../cJSON/cJSON_Utils.h \
+ ../cJSON/cJSON.h
+../../../../os/net/app-layer/coap/module-macros.h:
+../cJSON/cJSON_Utils.h:
+../cJSON/cJSON.h:
