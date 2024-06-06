53504536625538928e278a055f5c4eb0c80a1561
Vittori00 <lorenzo.vittori24@gmail.com>
2024-06-06 10:15:10 +0200

    aahhhhhhh

--- CoAP network/illumination sensor/build/nrf52840/dongle/obj/.deps/cJSON.c.d
+++
@@ -1,6 +0,0 @@
-build/nrf52840/dongle/obj/cJSON.o: ../cJSON/cJSON.c \
- ../../../../os/net/app-layer/coap/module-macros.h ../cJSON/cJSON.h
-
-../../../../os/net/app-layer/coap/module-macros.h:
-
-../cJSON/cJSON.h:
--- CoAP network/soil sensor/build/cooja/obj/.deps/cJSON.c.d
+++
@@ -1,4 +0,0 @@
-build/cooja/obj/cJSON.o: ../cJSON/cJSON.c \
- ../../../../os/net/app-layer/coap/module-macros.h ../cJSON/cJSON.h
-../../../../os/net/app-layer/coap/module-macros.h:
-../cJSON/cJSON.h:
